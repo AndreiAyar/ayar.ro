449e7b45bd14ffb2fb2469095170ec5cc8f3d094
rinxtzu <49005196+rinxtzu@users.noreply.github.com>
2020-09-09 19:06:42 +0300

    aa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import styled, { css } from "styled-components"
 import Logo from "./Logo"
+ 
+
+
 const MainHeader = styled.div`
   width: 100%;
   height: 70px;
